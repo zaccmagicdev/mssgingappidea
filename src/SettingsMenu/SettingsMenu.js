b56c708addac1b5b73d99326e79260c0b01fd66a
Zachary Wedel <zwede710@gmail.com>
2024-08-06 17:06:19 -0500

    UPDATE: Enhanced settings menu

--- src/SettingsMenu/SettingsMenu.js
+++ src/SettingsMenu/SettingsMenu.js
@@ -9,13 +9,17 @@ export const SettingsMenu = () => {
   return (
     <section className='settingsmenu'>
       <ul className='settingsmenu__list'>
+        <h2 className="settingsmenu__category-title">General Settings</h2>
         <li className='settingsmenu__setting'>
           <p className='settingsmenu__setting-title'>Background Colour</p>
           <button
-            className='settings__menusettingsmenu__setting-title-switch'
+            className='settings__menu-button menu__setting-title-switch'
             onClick={handleBackgroundThemeChange}>
-            {backgroundColor === 'dark' ? 'light' : 'dark'}
+            {backgroundColor === 'dark' ? 'Light' : 'Dark'}
           </button>
+        </li>
+        <li className='settingsmenu__setting'>
+          <p className="settingsmenu__setting-title">Font Size</p>
         </li>
       </ul>
     </section>
