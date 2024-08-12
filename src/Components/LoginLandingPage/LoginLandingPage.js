1fca1129caa28a7226ce64db42e8479e5449d716
Zachary Wedel <zwede710@gmail.com>
2024-08-12 14:40:28 -0500

    UPDATE: Added login portion of landing page

--- src/Components/LoginLandingPage/LoginLandingPage.js
+++ src/Components/LoginLandingPage/LoginLandingPage.js
@@ -21,6 +21,18 @@ function LoginLandingPage() {
         <label for="register-dob">
           <input type="date" id="register-dob"  max="2012-01-01" min="1941-01-01"/>
         </label>
+        <h1 className='login-landing-page__header'>Welcome back!</h1>
+        <div className='login-landing-page__other-options-container'>
+          <p className='login-landing-page__other-options-text'>You can also sign in through</p>
+          <button className='login-landing-page__link login-landing-page__google-link' />
+          <button className='login-landing-page__link login-landing-page__apple-link' />
+        </div>
+        <label for="login-username">
+          <input type='text' id='login-username' placeholder='Username' required minLength='5' maxLength='16' />
+        </label>
+        <label for="login-password">
+          <input type='password' id='login-password' placeholder='Password' required minLength='8' maxLength='20' />
+        </label>
       </form>
     </section>
   )
