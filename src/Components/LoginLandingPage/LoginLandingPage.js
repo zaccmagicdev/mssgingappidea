8fc00e0c8035cb2268b6f437f7e133d52f4a11b5
Zachary Wedel <zwede710@gmail.com>
2024-08-14 11:02:57 -0500

    FIX: Form containers now fit to main container size and styling elements

--- src/Components/LoginLandingPage/LoginLandingPage.js
+++ src/Components/LoginLandingPage/LoginLandingPage.js
@@ -10,38 +10,38 @@ function LoginLandingPage() {
         <div className='login-landing-page__section login-landing-page__section_register'>
           <form className='login-landing-page__form'>
             <h1 className='login-landing-page__header'>Create Account</h1>
-
             <p className='login-landing-page__text'>You can also create an account through</p>
             <div className='login-landing-page__other-options-container'>
               <button className='login-landing-page__link login-landing-page__external-link login-landing-page__google-link' />
               <button className='login-landing-page__link login-landing-page__external-link login-landing-page__apple-link' />
             </div>
-            <label for="register-username">
-              <input type='text' id='register-username' placeholder='Username' required minLength='5' maxLength='16' />
+            <label className='login-landing-page__label' for="register-username">
+              <input className='login-landing-page__input' type='text' id='register-username' placeholder='Username' required minLength='5' maxLength='16' />
             </label>
-            <label for="register-password">
-              <input type='p assword' id='register-password' placeholder='Password' required minLength='8' maxLength='20' />
+            <label className='login-landing-page__label' for="register-password">
+              <input className='login-landing-page__input' type='password' id='register-password' placeholder='Password' required minLength='8' maxLength='20' />
             </label>
-            <label for="register-dob">
+            <label className='login-landing-page__label' for="register-dob">
               <input type="date" id="register-dob" max="2012-01-01" min="1941-01-01" />
             </label>
+            <ButtonRipple className='login-landing-page__link' id='register-submit'>Sign Up</ButtonRipple>
           </form>
         </div>
         <div className='login-landing-page__section login-landing-page__section_login'>
           <form className='login-landing-page__form'>
             <h1 className='login-landing-page__header'>Welcome back!</h1>
-
             <p className='login-landing-page__other-options-text'>You can also sign in through</p>
             <div className='login-landing-page__other-options-container'>
               <button className='login-landing-page__link login-landing-page__external-link login-landing-page__google-link' />
               <button className='login-landing-page__link login-landing-page__external-link login-landing-page__apple-link' />
             </div>
-            <label for="login-username">
-              <input type='text' id='login-username' placeholder='Username' required minLength='5' maxLength='16' />
+            <label className='login-landing-page__label' for="login-username">
+              <input className='login-landing-page__input' type='text' id='login-username' placeholder='Username' required minLength='5' maxLength='16' />
             </label>
-            <label for="login-password">
-              <input type='password' id='login-password' placeholder='Password' required minLength='8' maxLength='20' />
+            <label className='login-landing-page__label' for="login-password">
+              <input className='login-landing-page__input' type='password' id='login-password' placeholder='Password' required minLength='8' maxLength='20' />
             </label>
+            <ButtonRipple className='login-landing-page__link' id='sign-in-submit'>Sign In</ButtonRipple>
           </form>
         </div>
         <div className='login-landing-page__toggle-container login-landing-page__toggle-container_left'>
