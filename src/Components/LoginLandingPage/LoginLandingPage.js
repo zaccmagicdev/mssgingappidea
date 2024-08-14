0933ef2da4359dbc8807cc57f471e04cb3ea75be
Zachary Wedel <zwede710@gmail.com>
2024-08-14 12:16:17 -0500

    UPDATE: Working on animations for swipe and working on positioning for landing page

--- src/Components/LoginLandingPage/LoginLandingPage.js
+++ src/Components/LoginLandingPage/LoginLandingPage.js
@@ -1,63 +1,134 @@
-import React from 'react'
-import './LoginLandingPage.css'
-import ButtonRipple from '../ButtonRipple/ButtonRipple'
+import React from "react";
+import "./LoginLandingPage.css";
+import ButtonRipple from "../ButtonRipple/ButtonRipple";
 
 function LoginLandingPage() {
-
   return (
-    <section className='login-landing-page__background'>
-      <div className='login-landing-page__container'>
-        <div className='login-landing-page__section login-landing-page__section_register'>
-          <form className='login-landing-page__form'>
-            <h1 className='login-landing-page__header'>Create Account</h1>
-            <p className='login-landing-page__text'>You can also create an account through</p>
-            <div className='login-landing-page__other-options-container'>
-              <button className='login-landing-page__link login-landing-page__external-link login-landing-page__google-link' />
-              <button className='login-landing-page__link login-landing-page__external-link login-landing-page__apple-link' />
+    <section className="login-landing-page__background">
+      <div className="login-landing-page__container">
+        <div className="login-landing-page__section login-landing-page__section_register">
+          <form className="login-landing-page__form">
+            <h1 className="login-landing-page__header">Create Account</h1>
+            <p className="login-landing-page__text">
+              You can also create an account through
+            </p>
+            <div className="login-landing-page__other-options-container">
+              <button className="login-landing-page__link login-landing-page__external-link login-landing-page__google-link" />
+              <button className="login-landing-page__link login-landing-page__external-link login-landing-page__apple-link" />
             </div>
-            <label className='login-landing-page__label' for="register-username">
-              <input className='login-landing-page__input' type='text' id='register-username' placeholder='Username' required minLength='5' maxLength='16' />
+            <label
+              className="login-landing-page__label"
+              for="register-username"
+            >
+              <input
+                className="login-landing-page__input"
+                type="text"
+                id="register-username"
+                placeholder="Username"
+                required
+                minLength="5"
+                maxLength="16"
+              />
             </label>
-            <label className='login-landing-page__label' for="register-password">
-              <input className='login-landing-page__input' type='password' id='register-password' placeholder='Password' required minLength='8' maxLength='20' />
+            <label
+              className="login-landing-page__label"
+              for="register-password"
+            >
+              <input
+                className="login-landing-page__input"
+                type="password"
+                id="register-password"
+                placeholder="Password"
+                required
+                minLength="8"
+                maxLength="20"
+              />
             </label>
-            <label className='login-landing-page__label' for="register-dob">
-              <input type="date" id="register-dob" max="2012-01-01" min="1941-01-01" />
+            <label className="login-landing-page__label" for="register-dob">
+              <input
+                type="date"
+                id="register-dob"
+                max="2012-01-01"
+                min="1941-01-01"
+              />
             </label>
-            <ButtonRipple className='login-landing-page__link' id='register-submit'>Sign Up</ButtonRipple>
+            <ButtonRipple
+              className="login-landing-page__link"
+              id="register-submit"
+            >
+              Sign Up
+            </ButtonRipple>
           </form>
         </div>
-        <div className='login-landing-page__section login-landing-page__section_login'>
-          <form className='login-landing-page__form'>
-            <h1 className='login-landing-page__header'>Welcome back!</h1>
-            <p className='login-landing-page__other-options-text'>You can also sign in through</p>
-            <div className='login-landing-page__other-options-container'>
-              <button className='login-landing-page__link login-landing-page__external-link login-landing-page__google-link' />
-              <button className='login-landing-page__link login-landing-page__external-link login-landing-page__apple-link' />
+        <div className="login-landing-page__section login-landing-page__section_login">
+          <form className="login-landing-page__form">
+            <h1 className="login-landing-page__header">Welcome back!</h1>
+            <p className="login-landing-page__other-options-text">
+              You can also sign in through
+            </p>
+            <div className="login-landing-page__other-options-container">
+              <button className="login-landing-page__link login-landing-page__external-link login-landing-page__google-link" />
+              <button className="login-landing-page__link login-landing-page__external-link login-landing-page__apple-link" />
             </div>
-            <label className='login-landing-page__label' for="login-username">
-              <input className='login-landing-page__input' type='text' id='login-username' placeholder='Username' required minLength='5' maxLength='16' />
+            <label className="login-landing-page__label" for="login-username">
+              <input
+                className="login-landing-page__input"
+                type="text"
+                id="login-username"
+                placeholder="Username"
+                required
+                minLength="5"
+                maxLength="16"
+              />
             </label>
-            <label className='login-landing-page__label' for="login-password">
-              <input className='login-landing-page__input' type='password' id='login-password' placeholder='Password' required minLength='8' maxLength='20' />
+            <label className="login-landing-page__label" for="login-password">
+              <input
+                className="login-landing-page__input"
+                type="password"
+                id="login-password"
+                placeholder="Password"
+                required
+                minLength="8"
+                maxLength="20"
+              />
             </label>
-            <ButtonRipple className='login-landing-page__link' id='sign-in-submit'>Sign In</ButtonRipple>
+            <ButtonRipple
+              className="login-landing-page__link"
+              id="sign-in-submit"
+            >
+              Sign In
+            </ButtonRipple>
+            <ButtonRipple
+              className="login-landing-page__link"
+              id="forgot-password"
+            >
+              Forgot your Password?
+            </ButtonRipple>
           </form>
         </div>
-        <div className='login-landing-page__toggle-container login-landing-page__toggle-container_left'>
-          <ButtonRipple className='login-landing-page__link'>Forgot your Password?</ButtonRipple>
-          <h1 className='login-landing-page__header'>Already with us?</h1>
-          <p className='login-landing-page__text'>Just sign in here!</p>
-        </div>
-        <div className='login-landing-page__toggle-container login-landing-page__toggle-container_right'>
-          <ButtonRipple className='login-landing-page__link'>Sign In</ButtonRipple>
-          <h1 className='login-landing-page__header'>New here?</h1>
-          <p className='login-landing-page__text'>Click below to register</p>
-          <ButtonRipple className='login-landing-page__link'>Sign Up</ButtonRipple>
+        <div className="login-landing-page__toggle-section">
+          <div className="login-landing-page__toggle">
+            <div className="login-landing-page__toggle-container login-landing-page__toggle-container_left">
+              <h1 className="login-landing-page__header">Already with us?</h1>
+              <p className="login-landing-page__text">Just sign in here!</p>
+            </div>
+            <div className="login-landing-page__toggle-container login-landing-page__toggle-container_right">
+              <ButtonRipple className="login-landing-page__link">
+                Sign In
+              </ButtonRipple>
+              <h1 className="login-landing-page__header">New here?</h1>
+              <p className="login-landing-page__text">
+                Click below to register
+              </p>
+              <ButtonRipple className="login-landing-page__link">
+                Sign Up
+              </ButtonRipple>
+            </div>
+          </div>
         </div>
       </div>
     </section>
-  )
+  );
 }
 
-export default LoginLandingPage
+export default LoginLandingPage;
