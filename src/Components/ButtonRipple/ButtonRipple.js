22732543b800bb7fc4763a09583fe399130e4885
Zachary Wedel <zwede710@gmail.com>
2024-08-15 15:30:04 -0500

    TEMPORARY FIX: Ripple effect appears in button but not where the user clicks, this is only a temporary fix

--- src/Components/ButtonRipple/ButtonRipple.js
+++ src/Components/ButtonRipple/ButtonRipple.js
@@ -14,26 +14,24 @@ export default function ButtonRipple({
     const rbuttons = document.querySelectorAll(".rbutton_ripple");
     rbuttons.forEach((rbutton) => {
       rbutton.onclick = ({ pageX, pageY, currentTarget }) => {
-
         let x =
           ((pageX - currentTarget.offsetLeft) * 100) /
           currentTarget.offsetWidth;
         let y =
           ((pageY - currentTarget.offsetTop) * 100) /
           currentTarget.offsetHeight;
-
         const ripple = document.createElement("span");
         const rippleColor = rbutton.dataset.ripple || "gray";
         ripple.classList.add("ripple-effect");
         ripple.style.background = rippleColor;
 
         rbutton.appendChild(ripple);
-        ripple.style.left = x + "%";
-        ripple.style.top = y + "%";
+        ripple.style.left = x
+        ripple.style.top = y
 
         setTimeout(() => {
           ripple.remove();
-        }, 600);
+        }, 300);
       };
     });
   }, []);
