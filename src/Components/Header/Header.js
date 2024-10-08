39c752848f0d40aa01cc0c19267089e081e96f8f
Zachary Wedel <zwede710@gmail.com>
2024-10-07 21:29:11 -0500

    UPDATE: Added PFP image in header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -4,11 +4,14 @@ import { NavBar } from "../NavBar/NavBar";
 
 
 export const Header = (props) => {
+
+ console.log(props.avatar)
+  
   return (
     <header className="header">
       <div className="header__container-title-toggle">
         <h1 className="header__greeting">Welcome back, {props.username}!</h1>
-        <img src={props.avatar} />
+        <img src={props.avatar} alt='Profile Avatar' />
       </div>
       <NavBar className="header__navigation" />
     </header>
